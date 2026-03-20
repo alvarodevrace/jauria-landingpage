17a62d47d79f00736fea645ff2cd8aa82d606892
AlvoCarrera <alcarreram@icloud.com>
2026-03-19 22:03:27 -0500

    chore: update landing ga4 id

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  ga4MeasurementId: 'G-1B578DCY0N',
+  ga4MeasurementId: 'G-E0GWKZSE3Y',
   supabaseUrl: 'https://bxatcmcommoqnxnyqchu.supabase.co',
   supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ4YXRjbWNvbW1vcW54bnlxY2h1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIyMTczNDQsImV4cCI6MjA4Nzc5MzM0NH0.rhB-c1NP5YIUzIgMptKlFM8RKewIDaapwM8DxwPyntg',
   sentryEnabled: true,
